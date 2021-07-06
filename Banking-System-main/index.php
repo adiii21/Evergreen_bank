e95b7ad87690d06eedf3a7e9291ff004a3d71931
Aditya Singh <85675018+adiii21@users.noreply.github.com>
2021-07-06 13:48:38 +0530

    Update index.php

--- Banking-System-main/index.php
+++ Banking-System-main/index.php
@@ -91,13 +91,13 @@
                 <h2>About Company Page</h2><br>
                 <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-               <div class="col-sm-12 col-md img text-center">
-                <img src="img/3.png" class="img-fluid pt-2">
-              </div>
+
                 <br><button class="btn btn-default btn-lg bg-success">Get in Touch</button>
               
               </div>
-
+               <div class="col-sm-12 col-md img text-center">
+                <img src="img/3.png" class="img-fluid pt-2">
+              </div>
                  </div>
             </div>
 
